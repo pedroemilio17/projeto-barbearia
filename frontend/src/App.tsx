2b10bda5cef55fba51f44224f5e68fabb0b7f9c0
Pedro Emilio <pedroemilio1703@gmail.com>
2026-02-12 16:10:05 -0400

    refactor(front): introduce app routing with protected /agendar flow

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,34 @@
-import { ThemeProvider } from './context/ThemeContext';
-import { CartProvider } from './context/CartContext';
-import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext";
+import { CartProvider } from "./context/CartContext";
+import { AuthProvider } from "./auth/AuthProvider";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Booking from "./pages/Booking";
+
+import RequireAuth from "./auth/RequireAuth";
 
 function App() {
   return (
     <ThemeProvider>
       <CartProvider>
-        <Home />
+        <AuthProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="/agendar"
+                element={
+                  <RequireAuth>
+                    <Booking />
+                  </RequireAuth>
+                }
+              />
+            </Routes>
+          </BrowserRouter>
+        </AuthProvider>
       </CartProvider>
     </ThemeProvider>
   );
